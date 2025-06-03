8f23e68532d2aeee6e6c5e655aa35fb479c598a0
nivkapadia <nivkpd@gmail.com>
2025-06-03 18:51:38 -0400

    updates

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,16 @@
 import Link from 'next/link'
+import { Suspense } from 'react'
 import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
+
+async function fetchMeals() {
+  const meals = await getMeals()
+  return meals
+}
 
 export default function MealPage() {
-  return <>
+  return <div>
     <header className={classes.header}>
       <h1>
         Delicious meals, created {' '}
@@ -17,7 +24,10 @@ export default function MealPage() {
       </p>
     </header >
     <main className={classes.main}>
-      <MealsGrid meals={[]} />
+      <Suspense>
+        fetching meals...
+      </Suspense>
+      <MealsGrid meals={fetchMeals} />
     </main>
-  </>
+  </div>
 }
--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -1,18 +1,20 @@
-import classes from './meal-item.module.css'
-import Image from "next/image"
-import Link from "next/link"
+import Link from 'next/link';
+import Image from 'next/image';
+
+import classes from './meal-item.module.css';
 
 export default function MealItem({ title, slug, image, summary, creator }) {
 	return (
 		<article className={classes.meal}>
 			<header>
-				<Image src={image} alt={title} fill />
+				<div className={classes.image}>
+					<Image src={image} alt={title} fill />
+				</div>
 				<div className={classes.headerText}>
 					<h2>{title}</h2>
 					<p>by {creator}</p>
 				</div>
 			</header>
-
 			<div className={classes.content}>
 				<p className={classes.summary}>{summary}</p>
 				<div className={classes.actions}>
@@ -20,5 +22,5 @@ export default function MealItem({ title, slug, image, summary, creator }) {
 				</div>
 			</div>
 		</article>
-	)
+	);
 }
